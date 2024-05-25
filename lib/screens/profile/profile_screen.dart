0ee7a00a9855ccdb674a8d8280c2a80471edb03f
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-05-25 10:30:45 +0900

    commit

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -38,12 +38,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ListView(
               padding: EdgeInsets.zero,
               children: [
-                const SizedBox(height: 150),
+                const SizedBox(height: 145),
                 Stack(
                   children: [
                     Container(
                       color: Colors.white,
-                      margin: const EdgeInsets.only(top: 50),
+                      margin: const EdgeInsets.only(top: 55),
                       height: MediaQuery.of(context).size.height,
                     ),
                     Padding(
@@ -51,7 +51,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       child: Stack(
                         children: [
                           const Padding(
-                            padding: EdgeInsets.only(top: 60),
+                            padding: EdgeInsets.only(top: 70),
                             child: ProfileSettingButton(),
                           ),
                           ProfileImage(
@@ -62,8 +62,81 @@ class _ProfileScreenState extends State<ProfileScreen> {
                               ),
                             ],
                           ),
+                          Container(
+                            margin: EdgeInsets.only(top: 110),
+                            width: 110,
+                            height: 30,
+                            child: Center(
+                              child: Text(
+                                '깃메잇',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(top: 160, right: 5),
+                                decoration: BoxDecoration(
+                                  color: Colors.lightBlue.shade300,
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                                  child: Text(
+                                    'Java',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(top: 160, right: 5),
+                                decoration: BoxDecoration(
+                                  color: Colors.lightBlue.shade300,
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                                  child: Text(
+                                    'Java',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(top: 160, right: 5),
+                                decoration: BoxDecoration(
+                                  color: Colors.lightBlue.shade300,
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                                  child: Text(
+                                    'Java',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 220),
+                      height: MediaQuery.of(context).size.height,
+                      decoration: BoxDecoration(border: Border.all()),
                     ),
                   ],
                 ),
